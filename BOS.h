518f5c1140d71e4e2fe822f84791fcce94e3ea38
Asaad K <masaadkaadan@gmail.com>
2019-07-09 22:07:10 +0300

    Adding H09R0 module

--- BOS.h
+++ BOS.h
@@ -22,7 +22,7 @@ enum PortNames_e{PC, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, PUSB, P_RS485};
 enum ButtonNames_e{B1=1, B2, B3, B4, B5, B6, B7, B8, B9, B10};
 enum PortStatus_e{FREE, MSG, STREAM, CLI, PORTBUTTON};
 enum UartDirection_e{NORMAL, REVERSED};
-enum modulePartNumbers_e{_H01R0=1, _P01R0, _H23R0, _H23R1, _H07R3, _H08R6, _H1BR6, _H12R0, _H13R7, _H0FR6, _H1AR2, _H0AR9, _H1DR1, _H1DR5, _H0BR4, _H18R0, _H26R0};
+enum modulePartNumbers_e{_H01R0=1, _P01R0, _H23R0, _H23R1, _H07R3, _H08R6, _H09R0, _H1BR6, _H12R0, _H13R7, _H0FR6, _H1AR2, _H0AR9, _H1DR1, _H1DR5, _H0BR4, _H18R0, _H26R0};
 enum IndMode_e{IND_OFF, IND_PING, IND_TOPOLOGY, IND_SHORT_BLINK};
 enum DMAStreamDirection_e{FORWARD, BACKWARD, BIDIRECTIONAL};
 enum buttonType_e{NONE=0, MOMENTARY_NO, MOMENTARY_NC, ONOFF_NO, ONOFF_NC};		/* NO: Naturally Open, NC: Naturally CLosed */
@@ -92,6 +92,9 @@ typedef enum { FMT_UINT8 = 1, FMT_INT8, FMT_UINT16, FMT_INT16, FMT_UINT32, FMT_I
 #endif
 #ifdef H08R6
 	#include "H08R6.h"	
+#endif
+#ifdef H09R0
+	#include "H09R0.h"	
 #endif
 #ifdef H1BR6
 	#include "H1BR6.h"	
@@ -322,7 +325,7 @@ extern uint8_t myID, bcastID;
 extern uint16_t myPN;
 extern uint8_t indMode;
 extern uint8_t N;
-extern const char modulePNstring[18][6];
+extern const char modulePNstring[19][6];
 extern uint8_t portStatus[NumOfPorts+1];
 extern uint16_t neighbors[NumOfPorts][2];
 extern uint8_t messageParams[20*(MAX_MESSAGE_SIZE-5)];
