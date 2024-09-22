895579666d85114ab7391372943bc2cc8f3f2325
ADEL\adool <adoolfaki@gmail.com>
2024-09-22 15:20:16 +0300

    Moving H0FR1 to G0 MCU

--- BOS.c
+++ BOS.c
@@ -62,7 +62,7 @@ char Processor_type(uint8_t module_name)
 {
 	if( module_name==_H1AR2 || module_name==_H23R3 ||
 		module_name==_H10R4 || module_name==_H0FR6 ||
-		module_name==_H41R6 || module_name==_H15R0 || module_name==_H0FR1||
+		module_name==_H41R6 || module_name==_H15R0 ||
 		module_name==_H1DR1 || module_name==_H07R3 )
 	{
 	  return 'F' ;
