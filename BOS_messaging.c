1bad5008cd0e0837f2d8eda0f8facb30d68ea988
ADEL\adool <adoolfaki@gmail.com>
2025-02-15 12:20:08 +0300

    Use ((writePxITMutex)) instead ((Send_BOS_Message))

--- BOS_messaging.c
+++ BOS_messaging.c
@@ -577,7 +577,8 @@ BOS_Status SendMessageFromPort(uint8_t port,uint8_t src,uint8_t dst,uint16_t cod
 		/* Transmit the message - single-cast */
 
 //		if(code == MSG_Acknowledgment_Accepted || code==MSG_rejected){
-			Send_BOS_Message(port,message,length + 4,cmd50ms,dst);
+//			Send_BOS_Message(port,message,length + 4,cmd50ms,dst);
+			writePxITMutex(port, message, length + 4, cmd50ms);
 //		}
 //		else{
 //
@@ -606,7 +607,8 @@ BOS_Status SendMessageFromPort(uint8_t port,uint8_t src,uint8_t dst,uint16_t cod
 		for(uint8_t p =1; p <= NumOfPorts; p++){
 			if((bcastRoutes[myID - 1] >> (p - 1)) & 0x01){
 				/* Transmit the message from this port */
-				Send_BOS_Message(p,message,length + 4,cmd50ms,dst);
+//				Send_BOS_Message(p,message,length + 4,cmd50ms,dst);
+				writePxITMutex(p, message, length + 4, cmd50ms);
 //				osDelay(200);
 //				if(rejected_FLAG == true)
 //					Send_BOS_Message(port,message,length + 4,cmd50ms,dst);
