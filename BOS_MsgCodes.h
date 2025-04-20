70e8203619612454fab0def5516f4da6bb4f7060
ADEL\adool <adoolfaki@gmail.com>
2025-04-20 12:17:44 +0300

    Organize IMU Message codes

--- BOS_MsgCodes.h
+++ BOS_MsgCodes.h
@@ -163,20 +163,11 @@
 #define CODE_H0AR9_STREAM_PIR                 509
 #define CODE_H0AR9_STREAM_STOP                510
 
-// H0BR4x
+/* H0BR4x */
 #define CODE_H0BR4_SAMPLE_GYRO                550
 #define CODE_H0BR4_SAMPLE_ACC                 551
 #define CODE_H0BR4_SAMPLE_MAG		          552
 #define CODE_H0BR4_SAMPLE_TEMP		          553
-#define CODE_H0BR4_RESULT_GYRO                554
-#define CODE_H0BR4_RESULT_ACC                 555
-#define CODE_H0BR4_RESULT_MAG		 	      556
-#define CODE_H0BR4_RESULT_TEMP		          557
-#define CODE_H0BR4_STREAM_GYRO	     		  558
-#define CODE_H0BR4_STREAM_ACC		    	  559
-#define CODE_H0BR4_STREAM_MAG		    	  560
-#define CODE_H0BR4_STREAM_TEMP		     	  561
-#define CODE_H0BR4_STREAM_STOP				  562
 
 // H0FR1x
 #define	CODE_H0FR1_ON						  750
