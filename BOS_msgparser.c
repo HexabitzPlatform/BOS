fddd8d85922d94b5c06f39bd65c2f4f5aa4c5d44
Mahmood_Reda <mr4141705@gmail.com>
2025-05-27 18:05:08 +0300

    Fix WriteRemote issue .

--- BOS_msgparser.c
+++ BOS_msgparser.c
@@ -870,7 +870,7 @@ void PxMessagingTask(void *argument){
 							}
 							else{
                                 /* Variable exists. Get its memory address */
-								temp32 =(bosVarRegister[cMessage[port - 1][shift] - REMOTE_BOS_VAR - 1] >> 16) + SRAM_BASE; /* + 0x10000*/
+                                temp32 =(bosVarRegister[cMessage[port - 1][shift] - REMOTE_BOS_VAR - 1] >> 16) + SRAM_BASE+ 0x10000;
                                 /* Send variable according to its format */
 								switch(MessageParams[0]) // requested format
 								{
@@ -1028,7 +1028,7 @@ void PxMessagingTask(void *argument){
 							/* Check variable index is within the limit of MAX_BOS_VARS */
 							if(cMessage[port - 1][shift] <= MAX_BOS_VARS){
 								/* Get var memory address */
-								temp32 =(bosVarRegister[cMessage[port - 1][shift] - 1] >> 16) + SRAM_BASE; /* + 0x10000*/
+								temp32 =(bosVarRegister[cMessage[port - 1][shift] - 1] >> 16) + SRAM_BASE+0x10000; // Get var memory addres
                                 /* Modify the variable or create a new one if it does not exist */
 								switch(cMessage[port - 1][1 + shift]) // requested format
 								{
